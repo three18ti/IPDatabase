a43b4138ab1d3a047769e788f2ebe572e3a2de19
Jon <root@raring64.nordstrom.net>
2013-08-16 07:24:51 -0600

    converted db to use int instead of string to store db. will likely need to update throughout where IPs are pulled.

--- gen_sql2.pl
+++ gen_sql2.pl
@@ -13,12 +13,14 @@ sub long2ip {
     return inet_ntoa(pack("N*", shift));
 }
 
-my $ip_id = 13;
+my $ip_id = 1;
 
 my (@ip_insert, @ip_subnet_insert);
 
-foreach ( 6 .. 255 ) {
-    my $ip = ip2long "192.168.0.$_";
+#foreach ( 6 .. 255 ) {
+foreach (qw( 192.168.0.2 192.168.0.3 192.168.0.4 192.168.0.5 10.10.0.2 10.10.0.3 10.10.0.4 10.10.0.5 172.0.10.2 172.0.10.3 172.0.10.4 172.0.10.5 )){
+
+    my $ip = ip2long $_;
     push @ip_insert, "INSERT INTO \"ip\" VALUES($ip_id, $ip, '' );";
     push @ip_subnet_insert, "INSERT INTO \"ip_subnet\" VALUES(1, $ip_id);";
     $ip_id++;
@@ -26,3 +28,4 @@ foreach ( 6 .. 255 ) {
 
 map { say } @ip_insert;
 map { say } @ip_subnet_insert;
+
